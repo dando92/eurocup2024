669e0b65d5b604ac1fef4078653057c4aedb2cf5
Andrea Montanaro <andrea@andreamontanaro.it>
2024-05-23 12:57:49 +0200

    add colors

--- TournamentViewer/tailwind.config.js
+++ TournamentViewer/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blu: '#359DD5',
+        bianco: '#F3F9FD',
+        giallo: '#FFCC07',
+        nero: '#142739',
+        grigio: '#E9E9E9',
+        lower: '#2E9BD5',
+        middle: '#256CAE',
+        upper: '#0E1928',
+        grigioQualifica: '#444443',
+        gialloQualifica: '#FFC505',
+      }
+    },
   },
   plugins: [],
 }
