dcc6c7daaec7418eb0b3f27eebbc32cfe8707287
Andrea Montanaro <andrea@andreamontanaro.it>
2024-05-23 12:46:01 +0200

    scaffold frontend project

---
+++ TournamentViewer/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-bold text-3xl">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        voluptatum, laudantium voluptatibus accusamus pariatur sit doloribus
+        expedita porro hic consectetur incidunt enim ipsam dicta suscipit
+        maiores molestias, fugit quia quibusdam?
+      </p>
+    </div>
+  );
+}
+
+export default App;
