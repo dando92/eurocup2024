a9d6237bedaa36a75829365a82cde2ed1dadb219
alessandro.dore <alessandro.dore@inpeco.com>
2024-05-24 10:06:14 +0200

    Removed external lib

--- TournamentManager/Services/StandingService.cs
+++ TournamentManager/Services/StandingService.cs
@@ -1,14 +1,8 @@
-﻿using Fleck;
-using System.Text.Json;
-using TournamentManager.Requests;
-
-namespace TournamentManager.Services
+﻿namespace TournamentManager.Services
 {
     public class StandingService : BackgroundService
     {
         private IRawStandingSubscriber _subscriber;
-
-        WebSocketServer _server;
 
         public string IpAddress { get; }
         public ushort Port { get; }
@@ -23,23 +17,9 @@ namespace TournamentManager.Services
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _server = new WebSocketServer($"ws://{IpAddress}:{Port}/");
-
-            _server.Start(conn =>
-            {
-                conn.OnOpen = () =>
-                { 
-                    //TODO: Necessary?
-                };
-                conn.OnMessage = message =>
-                {
-                    _subscriber.OnNewStanding(JsonSerializer.Deserialize<PostStandingRequest>(message));
-                };
-                conn.OnClose = () =>
-                {
-                    //TODO: Necessary?
-                };
-            });
+            //TODO: Open client towards NodeJs app
+            //OnReceiveMessage ->
+            //_subscriber.OnNewStanding(JsonSerializer.Deserialize<PostStandingRequest>(message));
 
             return Task.CompletedTask;
         }
