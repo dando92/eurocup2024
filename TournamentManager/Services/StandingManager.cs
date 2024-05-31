c4df4c8563f14ae4b5cc12570a98f4ef29738623
alessandro.dore <alessandro.dore@inpeco.com>
2024-05-31 15:56:11 +0200

    Added logs for standings

--- TournamentManager/Services/StandingManager.cs
+++ TournamentManager/Services/StandingManager.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.Components.Forms;
 using System.Globalization;
+using System.Numerics;
 using System.Text.RegularExpressions;
 using TournamentManager.Contexts;
 using TournamentManager.DbModels;
@@ -10,6 +12,7 @@ namespace TournamentManager.Services
     {
         private readonly ITournamentCache _cache;
         private readonly IMatchUpdate _hub;
+        private readonly ILogUpdate _logHub;
         private readonly IGenericRepository<Standing> _standingRepo;
         private readonly IGenericRepository<Song> _songRepo;
         private readonly IGenericRepository<Player> _playerRepo;
@@ -18,13 +21,15 @@ namespace TournamentManager.Services
             IGenericRepository<Player> playerRepo,
             IGenericRepository<Standing> standingRepo,
             ITournamentCache cache,
-            IMatchUpdate matchUpdate)
+            IMatchUpdate matchUpdate,
+            ILogUpdate logHub)
         {
             _songRepo = songRepo;
             _playerRepo = playerRepo;
             _cache = cache;
             _standingRepo = standingRepo;
             _hub = matchUpdate;
+            _logHub = logHub;
         }
 
         public void AddStanding(Score score)
@@ -41,8 +46,11 @@ namespace TournamentManager.Services
 
             //Player or song not registered, do nothing
             if (song == null || player == null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "No song or player found for standing" });
                 return;
-
+            }
+                
             Standing standing = new Standing()
             {
                 Percentage = double.Parse(score.FormattedScore, CultureInfo.InvariantCulture),
@@ -57,24 +65,36 @@ namespace TournamentManager.Services
                .FirstOrDefault();
 
             if (duplicate != null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = $"duplicate found for {song.Title} - {player.Name}" });
                 return;
-
+            }
+                
             AddStanding(standing);
         }
 
         public void AddStanding(Standing standing)
         {
             if (_cache.ActiveMatch == null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = $"No active match during standing push" });
                 return;
-
+            }
+                
             if (standing.SongId == 0 || standing.PlayerId == 0)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "No song or player found for standing" });
                 return;
-
+            }
+                
             Round round = _cache.GetRoundBySongId(standing.SongId);
 
             if (round == null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "Round not found for active match" });
                 return;
-
+            }
+                
             var playerInActiveMatch = _cache.ActiveMatch.PlayerInMatches.Where(pim => pim.PlayerId == standing.PlayerId).FirstOrDefault();
             var songInActiveMatch = _cache.ActiveMatch.SongInMatches.Where(pim => pim.SongId == standing.SongId).FirstOrDefault();
 
@@ -84,22 +104,31 @@ namespace TournamentManager.Services
             standing.RoundId = round.Id;
 
             if (round.Standings.Where(s => s.PlayerId == standing.PlayerId && s.SongId == standing.SongId).Count() > 0)
-                return;
-            
-            _standingRepo.Add(standing);
-            round.Standings.Add(standing);
-
-            if (round.IsComplete())
             {
-                if(!_cache.ActiveMatch.IsManualMatch)
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "Skip standing, duplicate" });
+                return;
+            }
+            try
+            {
+                _standingRepo.Add(standing);
+                round.Standings.Add(standing);
+
+                if (round.IsComplete())
                 {
-                    round.Standings.Recalc();
+                    if (!_cache.ActiveMatch.IsManualMatch)
+                    {
+                        round.Standings.Recalc();
 
-                    foreach (var recalcStanding in round.Standings)
-                        _standingRepo.Update(recalcStanding);
+                        foreach (var recalcStanding in round.Standings)
+                            _standingRepo.Update(recalcStanding);
+                    }
+
+                    _hub?.OnMatchUpdate(new MatchUpdateDTO() { MatchId = _cache.ActiveMatch.Id, PhaseId = _cache.ActiveMatch.PhaseId, DivisionId = _cache.ActiveMatch.Phase.DivisionId });
                 }
-
-                _hub?.OnMatchUpdate(new MatchUpdateDTO() { MatchId = _cache.ActiveMatch.Id, PhaseId = _cache.ActiveMatch.PhaseId, DivisionId = _cache.ActiveMatch.Phase.DivisionId });
+            }
+            catch(Exception ex)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Exception = ex.Message });
             }
         }
 
@@ -108,24 +137,37 @@ namespace TournamentManager.Services
             bool edited = false;
 
             if (_cache.ActiveMatch == null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = $"No active match during standing edit" });
                 return edited;
-
+            }
+                
             Round round = _cache.GetRoundBySongId(songId);
 
             if (round == null)
-                return false;
+            { 
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "Round not found for active match" });
+                return edited;
+            }
 
-            foreach (var standing in round.Standings)
+            try
             {
-                if (standing.PlayerId == playerdId && standing.SongId == songId)
+                foreach (var standing in round.Standings)
                 {
-                    standing.Percentage = percentage;
-                    standing.Score = score;
-                    
-                    _standingRepo.Update(standing);
+                    if (standing.PlayerId == playerdId && standing.SongId == songId)
+                    {
+                        standing.Percentage = percentage;
+                        standing.Score = score;
 
-                    edited = true;
+                        _standingRepo.Update(standing);
+
+                        edited = true;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Exception = ex.Message });
             }
 
             return edited;
@@ -136,21 +178,34 @@ namespace TournamentManager.Services
             bool removed = false;
 
             if (_cache.ActiveMatch == null)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = $"No active match during standing edit" });
                 return removed;
+            }
 
             Round round = _cache.GetRoundBySongId(songId);
 
             if (round == null)
-                return false;
-
-            foreach (var standing in round.Standings)
             {
-                if (standing.PlayerId == playerdId && standing.SongId == songId)
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Message = "Round not found for active match" });
+                return removed;
+            }
+
+            try
+            {
+                foreach (var standing in round.Standings)
                 {
-                    _standingRepo.DeleteById(standing.Id);
-                    round.Standings.Remove(standing);
-                    removed = true;
+                    if (standing.PlayerId == playerdId && standing.SongId == songId)
+                    {
+                        _standingRepo.DeleteById(standing.Id);
+                        round.Standings.Remove(standing);
+                        removed = true;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                _logHub.OnLogUpdate(new LogUpdateDTO() { Exception = ex.Message });
             }
 
             return removed;
