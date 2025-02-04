ec8b8c9857d38805fde099e9ccc933e8c158441d
alessandro-inpeco-WnMSdr <alessandro.dore@inpeco.com>
2025-02-04 17:33:04 +0100

    Added scoring system

---
+++ tournament_manager_backend/src/services/IScoringSystem.ts
@@ -0,0 +1,78 @@
+import { Standing } from "src/crud/entities";
+
+class IScoringSystem {
+    constructor() {
+        if (this.constructor === IScoringSystem) {
+            throw new Error("Cannot instantiate an abstract class.");
+        }
+    }
+
+    getName() {
+        throw new Error("Method 'Name' should be implemented.");
+    }
+
+    getDescription() {
+        throw new Error("Method 'Description' should be implemented.");
+    }
+
+    recalc(standings: Standing[]) {
+        throw new Error("Method 'recalc' should be implemented.");
+    }
+}
+
+class EurocupScoreCalculator extends IScoringSystem {
+    constructor() {
+        super();
+    }
+
+    getName() {
+        return "EurocupScoreCalculator";
+    }
+
+    getDescription() {
+        return "Fail count 0";
+    }
+
+    recalc(standings: Standing[]) {
+        let maxPoints = standings.length;
+        let orderedStandings = standings.filter(s => !s.score.isFailed).sort((a, b) => b.score.percentage - a.score.percentage);
+        let tieCount = 0;
+
+        for (let i = 0; i < orderedStandings.length; i++) {
+            orderedStandings[i].points = Math.floor(maxPoints);
+
+            if (i + 1 < orderedStandings.length) {
+                if (orderedStandings[i].score.percentage > orderedStandings[i + 1].score.percentage) {
+                    if (tieCount > 0) {
+                        maxPoints -= tieCount;
+                        tieCount = 0;
+                    }
+                    maxPoints--;
+                } else if (orderedStandings[i].score.percentage === orderedStandings[i + 1].score.percentage) {
+                    tieCount++;
+                }
+            }
+        }
+    }
+}
+
+class FinalsCalculator extends IScoringSystem {
+    constructor() {
+        super();
+    }
+
+    getName() {
+        return "EurocupFinalsScoringSystem";
+    }
+
+    getDescription() {
+        return "First to n";
+    }
+
+    recalc(standings: Standing[]) {
+        let orderedStandings = standings.sort((a, b) => b.score.percentage - a.score.percentage).sort((a, b) => b.score.isFailed ? 0 : 1);
+
+        orderedStandings[0].points = 1;
+        orderedStandings[1].points = 0;
+    }
+}
