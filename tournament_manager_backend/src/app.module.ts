cca4f8dee709b4cdd3c66064efbd53f211707de2
alessandro-inpeco-WnMSdr <alessandro.dore@inpeco.com>
2025-02-12 12:44:45 +0100

    Added crud module and imported into main app

--- tournament_manager_backend/src/app.module.ts
+++ tournament_manager_backend/src/app.module.ts
@@ -1,9 +1,5 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:175172732.
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Entities } from './crud/entities';
-import { Services } from './crud/services';
-import { Controllers } from './crud/controllers';
 import { BackwardCompatibilityController } from './backwardcompatibility.controller';
 import { MatchManager } from './services/match.manager';
 import { StandingManager } from './services/standing.manager';
@@ -13,57 +9,22 @@ import { MatchGateway } from './gateways/match.gateway'
 import { ScoringSystemProvider } from './services/IScoringSystem';
 import { GameGateway } from './gateways/game.gateway';
 import { LiveScoreGateway } from './gateways/live.score.gateway'
-
-const controllers = [
-  Controllers[0],
-  Controllers[1],
-  Controllers[2],
-  Controllers[3],
-  Controllers[4],
-  Controllers[5],
-  Controllers[6],
-  Controllers[7],
-  Controllers[8],
-  Controllers[9],
-  Controllers[10],
-  Controllers[11],
-  Controllers[12],
-  BackwardCompatibilityController
-]
-
-const services = [
-  Services[0],
-  Services[1],
-  Services[2],
-  Services[3],
-  Services[4],
-  Services[5],
-  Services[6],
-  Services[7],
-  Services[8],
-  Services[9],
-  TournamentCache,
-  SongRoller,
-  MatchManager,
-  StandingManager,
-  MatchGateway,
-  ScoringSystemProvider,
-  GameGateway,
-  LiveScoreGateway
-]
-
+import { CrudModule } from './crud/crud.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'tournament.db',
-      entities: Entities,
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature(Entities),
+  imports: [ CrudModule ],
+  controllers:[
+    BackwardCompatibilityController
   ],
-  controllers: controllers,
-  providers: services,
+  providers: [
+    TournamentCache,
+    SongRoller,
+    MatchManager,
+    StandingManager,
+    MatchGateway,
+    ScoringSystemProvider,
+    GameGateway,
+    LiveScoreGateway
+  ],
 })
 export class AppModule { }
